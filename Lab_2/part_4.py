4bfedfa3d711f95ada3e000a061e8c3ca2585e0d
spencerperley <spencerp11338@gmail.com>
2022-01-19 11:28:11 -0800

    fixed some spelling

--- Lab_2/part_4.py
+++ Lab_2/part_4.py
@@ -1,1 +1,1 @@
-print("The tempeture in farenhight is " + str((int(input("Enter a tempeture in celcius to be converted: ")) * 1.8) + 32))
+print("The temperature in fahrenheit is " + str((int(input("Enter a temperature in celsius to be converted: ")) * 1.8) + 32))
